b5a90f2cc8ee30f066512ec4e7bfdfbdddd648e7
Siulok Tang <90478323+stang1207@users.noreply.github.com>
2022-03-11 14:01:07 -0500

    updated frontend

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
+const restaurantPath = require('path').join(__dirname, '../data/data.json');
 
 // GET route for list of review
 router.get('/', (req, res) => {
   // reads the JSON file and returns the data
-  fs.readFile('../data/data.json', 'utf8', (err, data) => {
+  fs.readFile(restaurantPath, 'utf8', (err, data) => {
     const reviewData = JSON.parse(data);
-    res.json(reviewData);
+    console.log(reviewData);
+    return res.json(reviewData);
   });
 });
 
 // GET route for a review with a specific ID
 router.get('/:id', (res, req) => {
-  fs.readFile('../data/data.json', 'utf8', (err, data) => {
+  fs.readFile(restaurantPath, 'utf8', (err, data) => {
     const reviewData = JSON.parse(data);
     // look for the review with the id
     const foundReview = reviewData.find(
@@ -32,21 +34,22 @@ router.get('/:id', (res, req) => {
 // POST route for a submitting a review
 router.post('/', (req, res) => {
   // reads the JSON file
-  fs.readFile('../data/data.json', 'utf8', (err, data) => {
+  fs.readFile(restaurantPath, 'utf8', (err, data) => {
     const reviewData = JSON.parse(data);
     const newReview = {
       id: uuidv4(),
       author: req.body.author,
-      comment: req.body.comment
-      }
-    });
-    reviewData.push(newReview);
-    // writes to the JSON file to add the newly uploaded video
-    fs.writeFile('../data/data.json', JSON.stringify(reviewData), () => {
-      res.json({message: 'data written to file successfully', data: reviewData});
+      comment: req.body.comment,
+    };
+  });
+  reviewData.push(newReview);
+  // writes to the JSON file to add the newly uploaded video
+  fs.writeFile(restaurantPath, JSON.stringify(reviewData), () => {
+    res.json({
+      message: 'data written to file successfully',
+      data: reviewData,
     });
   });
-
-
+});
 
 module.exports = router;
