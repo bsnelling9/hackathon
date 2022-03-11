e9a6c21cb9c7884a91d3b4050f3ef2c16fc7159b
Siulok Tang <90478323+stang1207@users.noreply.github.com>
2022-03-11 12:49:50 -0500

    front-end styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
-import './App.css';
-import Navbar from "./component/Navbar";
-import Hero from "./component/Hero";
-import Content from './component/Content';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './component/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
-
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element = {<Home />}/>
-          </Routes>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Hero from '../component/Hero/Hero';
+import Content from '../component/Content/Content';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <section className="home">
+        <Hero />
+        <Content />
+      </section>
+    );
+  }
+}
